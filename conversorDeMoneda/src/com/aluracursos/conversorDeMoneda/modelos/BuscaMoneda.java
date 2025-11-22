ed7cf5cce0cbfb4498ce5728a4fe93ff4d48508c
cars213 <carlosrendons213@gmail.com>
2025-11-22 14:41:03 -0500

    Delete conversorDeMoneda directory

--- conversorDeMoneda/src/com/aluracursos/conversorDeMoneda/modelos/BuscaMoneda.java
+++
@@ -1,7 +0,0 @@
-package com.aluracursos.conversorDeMoneda.modelos;
-/*Record encargado de recibir los datos de dolar,real brasileño, peso colombiano y peso argentino*/
-public record BuscaMoneda(double USD,
-                          double COP,
-                          double ARS,
-                          double BRL) {
-}
